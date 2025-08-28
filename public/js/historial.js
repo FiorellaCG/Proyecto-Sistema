c123eb0358b5d76a080ec5916509cf4eb3198cf6
noelia-munozlobo <nmunoz.lobofwd@gmail.com>
2025-08-28 11:28:58 -0600

    historial

---
+++ public/js/historial.js
@@ -0,0 +1,22 @@
+async function cargarPermisos() {
+  try {
+    const res = await fetch("http://localhost:3000/permisos");
+    const permisos = await res.json();
+    const tabla = document.getElementById("tablaPermisos");
+    tabla.innerHTML = "";
+    permisos.forEach(p => {
+      const fila = document.createElement("tr");
+      fila.innerHTML = `
+        <td>${p.nombre}</td>
+        <td>${p.fecha}</td>
+        <td>${p.motivo}</td>
+        <td>${p.computadora}</td>
+      `;
+      tabla.appendChild(fila);
+    });
+  } catch (error) {
+    console.error("Error al cargar permisos:", error);
+  }
+}
+// Ejecutar la carga al abrir la página
+cargarPermisos();
